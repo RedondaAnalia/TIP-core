50f83c6f6ad24c960a9ae78c7112928d126d9031
Saba <sabaliauskaspablo@gmail.com>
2019-03-30 18:09:45 -0300

    :tada: firt commit of proyect back-end

---
+++ controller/userController.js
@@ -0,0 +1,82 @@
+// userController.js
+// Import contact model
+User = require('../model/userModel');
+// Handle index actions
+exports.index = function (req, res) {
+    User.get(function (err, user) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Users retrieved successfully",
+            data: user
+        });
+    });
+};
+// Handle create user actions
+exports.new = function (req, res) {
+    var user = new User();
+    user.name = req.body.name ? req.body.name : user.name;
+    user.gender = req.body.gender;
+    user.email = req.body.email;
+    user.phone = req.body.phone;
+    user.experience = 0;
+    user.level = 0;
+// save the user and check for errors
+    user.save(function (err) {
+        // if (err)
+        //     res.json(err);
+res.json({
+            message: 'New user created!',
+            data: user
+        });
+    });
+};
+// Handle view user info
+exports.view = function (req, res) {
+    User.findById(req.params.user_id, function (err, user) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'Contact details loading..',
+            data: contact
+        });
+    });
+};
+// Handle update user info
+exports.update = function (req, res) {
+User.findById(req.params.user_id, function (err, contact) {
+        if (err)
+            res.send(err);
+user.name = req.body.name ? req.body.name : user.name;
+        user.gender = req.body.gender;
+        user.email = req.body.email;
+        user.phone = req.body.phone;
+// save the contact and check for errors
+        user.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'User Info updated',
+                data: contact
+            });
+        });
+    });
+};
+// Handle delete contact
+exports.delete = function (req, res) {
+    User.remove({
+        _id: req.params.user_id
+    }, function (err, user) {
+        if (err)
+            res.send(err);
+res.json({
+            status: "success",
+            message: 'Contact deleted'
+        });
+    });
+};
---
+++ model/userModel.js
@@ -0,0 +1,28 @@
+// usertModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: String,
+    phone: String,
+    pets: [Number],
+    experience: Number,
+    level: Number,
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+// Export User model
+var User = module.exports = mongoose.model('user', userSchema);
+module.exports.get = function (callback, limit) {
+    User.find(callback).limit(limit);
+}
+
