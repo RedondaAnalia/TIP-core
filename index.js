472a4eb059c39bbb4583a6e7d0247b6eb733831b
RedondaAnalia <a.redonda89@gmail.com>
2019-04-04 17:35:51 -0300

    Agrego habilitacion de CORS

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
 
 //Conexion con BBDD
 mongoose.connection.openUri('mongodb://localhost:27017/petHeroesDB',
