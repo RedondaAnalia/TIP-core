f69c0b15b7ec15af0e2d104451e152a56c085051
Pablo Nicolas <sabaliauskaspablo@gmail.com>
2019-04-15 18:48:12 -0300

    Revert ":hammer: refactor userController"

--- repository/user.repository.js
+++ repository/user.repository.js
@@ -1,37 +1,10 @@
-const User = require('../model/userModel');
+let User = require('../model/userModel');
+
 
 exports.findAll = () => {
-  return User.find({ }, '').populate('pets');
-};
+    return User.find({ }, '').populate('pets');
+}
 
 exports.countAll = () => {
-  return User.count({});
-};
-
-exports.new = (u) => {
-  return new User({
-    name : u.name,
-    gender : u.gender,
-    email : u.email,
-    phone : u.phone
-  }).save();
-};
-
-exports.findById = (id) => {
-  return User.findById(id);
-};
-
-exports.update = (u) => {
-  this.findById(u._id).then((user) => {
-    if (!user)
-      return null;
-    user.name = u.name ? u.name : user.name;
-    user.gender = u.gender ? u.gender : user.gender;
-    user.email = u.email ? u.email : user.email;
-    user.phone = u.phone ? u.phone : user.phone;
-    // save the user and check for errors
-    return user.save();
-  }).catch((err) => {
-    return err;
-  });
-};
+    return User.count({})
+}
