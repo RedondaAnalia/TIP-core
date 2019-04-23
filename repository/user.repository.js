b3d241263b35b0ed69f0e1a1dbd0dc1996e64c9f
RedondaAnalia <a.redonda89@gmail.com>
2019-04-23 17:31:30 -0300

    fix addPet

--- repository/user.repository.js
+++ repository/user.repository.js
@@ -28,7 +28,7 @@ exports.new = (u) => {
 };
 
 exports.findById = (id) => {
-  return User.findById({id});
+  return User.findById({_id:id});
 };
 
 exports.update = (u) => {
@@ -53,22 +53,10 @@ exports.findByEmail = (email) => {
   return User.findOne({email}).populate('pets');
 };
 
-exports.addPet= (user_id,pet) => {
-  let userFound;
-  return this.findById(user_id)
-    .then((user) => {
-      if (!user) {
-        const err = new Error('User Exception: User not found');
-        throw err;
-      }
-      userFound=user;
-      return petRepository.new(pet);
-    })
-    .then(pet => {
-      userFound.pets.push(pet);
-    })
-    .then(() => {
-      return User.update(userFound);
-    });
+exports.addPet= (user_id,pet) => {  
+  return petRepository.new(pet).then((res)=>
+    User.findOneAndUpdate({_id: user_id}, {$addToSet: {pets: res}}
+))
 };
 
+//
